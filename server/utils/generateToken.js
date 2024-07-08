918cf1b00c6f03798c2d99a1937a884eaf3e0253
ZohaibZahid12 <144813066+ZohaibZahid12@users.noreply.github.com>
2024-07-08 16:52:05 +0500

    added generate token function in utils

---
+++ server/utils/generateToken.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+module.exports = generateToken;
